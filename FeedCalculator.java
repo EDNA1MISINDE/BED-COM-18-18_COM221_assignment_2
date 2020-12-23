2f3ad19ec232e1a8d5b4cb639b903d1a6f428bd0
EDNA1MISINDE <ednamisinde@gmail.com>
2020-12-23 04:11:16 +0200

    initial commit

--- FeedCalculator.java
+++ FeedCalculator.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -24,26 +25,54 @@ public class FeedCalculator extends Application{
 
         Button calculateBT = new Button("Calculate");
 
+       /* Label maizeLB = new Label("Maize:");
+        Label soyaLB = new Label("Soya:");
+        Label groundnutLB = new Label("Groundnut:");
+        Label saltLB = new Label("Salt:");*/
+
     public void start(Stage primaryStage){
 
        //pane.setRight(getVBox());
 
-        //GridPane pane = new pane();
+        GridPane gridPane = new GridPane();
 
         Pane pane = new Pane();
+ //pane.setPadding(new Insets(5, 5, 5, 5));
+ //Text text1 = new Text(20, 20, "Programming is fun");
 
         maizeTF.setAlignment(Pos.BOTTOM_RIGHT);
         soyaTF.setAlignment(Pos.BOTTOM_RIGHT);
         groundnutTF.setAlignment(Pos.BOTTOM_RIGHT);
         saltTF.setAlignment(Pos.BOTTOM_RIGHT);
 
-        Scene scene = new Scene(pane, 200, 200);
+       /* maizeLB.setAlignment(Pos.BOTTOM_LEFT);
+        soyaLB.setAlignment(Pos.BOTTOM_LEFT);
+        groundnutLB.setAlignment(Pos.BOTTOM_LEFT);
+        saltLB.setAlignment(Pos.BOTTOM_LEFT);*/
+
+
+        Label label = new Label();
+         VBox vBox = new VBox(5);
+     //vBox.getChildren().addAll(maizeTF, soyaTF, groundnutTF, saltTF, calculateBT);
+     //vBox.getChildren().add(label);
+     //label.setAlignment(Pos.BOTTOM_RIGHT);
+     vBox.getChildren().addAll(new Label("Maize:"), maizeTF, new Label("Soya:"), soyaTF, new Label("Groundnut:"), groundnutTF, new Label("Salt:"), saltTF, calculateBT);
+    // VBox getVBox(){
+          //VBox vBox = new VBox(15);
+ vBox.setSpacing(10);
+ //vBox.setAlignment(Pos.BOTTOM_RIGHT);
+ vBox.setPadding(new Insets(15, 5, 5, 5));
+ //return vBox;
+    // }
+
+        Scene scene = new Scene(vBox);
             //setting the title
      primaryStage.setTitle("FeedCalculator"); 
      primaryStage.setResizable(false);
      //putting the scene in the stage
      primaryStage.setScene(scene); 
      primaryStage.show();
+       
 
       calculateBT.setOnAction(new EventHandler<ActionEvent>(){
         public void handle(ActionEvent e){
@@ -51,18 +80,10 @@ public class FeedCalculator extends Application{
         }
         });
     }
-     VBox getVBox(){
-        VBox vBox = new VBox();
- vBox.setSpacing(10);
- //vBox.setAlignment(Pos.BOTTOM_RIGHT);
- vBox.setPadding(new Insets(15, 5, 5, 5));
- vBox.getChildren().add(new Label("Maize:"));
- vBox.getChildren().add(maizeTF);
- return vBox;
-    } 
+     
 
 
-// vBox.getChildren().addAll(maizeTF, soyaTF, groundnutTF, saltTF, calculateBT);
+
  //vBox.add(new Label("Maize:"));
  //vBox.getChildren().add(maizeTF);
 
